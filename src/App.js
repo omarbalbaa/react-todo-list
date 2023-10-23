17516296caa6cf22bd352a08018818775c5a3580
Balbaa <omareidbalbaa@gmail.com>
2023-10-23 19:00:57 +0300

    delete task icon added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const handleAddTask = () => {
     const text = inputRef.current.value;
-    const newTask = {completed: false, text}
+    const newTask = { completed: false, text };
     setTasks([...tasks, newTask]);
     inputRef.current.value = "";
   };
@@ -16,15 +16,32 @@ function App() {
     const newTasks = [...tasks];
     newTasks[index].completed = !newTasks[index].completed;
     setTasks(newTasks);
-  }
+  };
+
+  const handleDeleteItem = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
 
   return (
     <div className="App">
-      <h2>To Do List</h2>
+      <h2>📝 To Do List</h2>
       <div className="to-do-container">
         <ul>
-          {tasks.map(({text, completed}, index) => {
-            return <li className={completed ? "done" : ""} key={index} onClick={()=>handleItemDone(index)}>{text}</li>;
+          {tasks.map(({ text, completed }, index) => {
+            return (
+              <div className="item">
+                <li
+                  className={completed ? "done" : ""}
+                  key={index}
+                  onClick={() => handleItemDone(index)}
+                >
+                  {text}
+                </li>
+                <span className="trash" onClick={() => handleDeleteItem(index)}>❌</span>
+              </div>
+            );
           })}
         </ul>
         <input ref={inputRef} placeholder="Enter task..." />
